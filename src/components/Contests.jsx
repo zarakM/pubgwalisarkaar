ff07005f1d0387faecce4c63003f84f2187dd5dc
zarakM <mughalzarak@gmail.com>
2019-02-21 01:00:25 +0500

    contest joining corsoma done

--- src/components/Contests.jsx
+++ src/components/Contests.jsx
@@ -11,11 +11,19 @@ class Contests extends Component {
     constructor() {
         super()
         this.state = {
-            contests: []
+            contests: [],
+            join: "Join",
+            user_id: null
         }
+        let com = this
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                com.setState({ user_id: user.uid })
+            }
+        })
     }
     componentDidMount() {
-        let com = this;
+        let com = this
         firebase
             .database()
             .ref()
@@ -38,30 +46,68 @@ class Contests extends Component {
                     contests: com.state.contests
                 });
             });
+
+    }
+
+    Join(entry, id, e) {
+        e.preventDefault()
+        let com = this
+        let joined = false
+        firebase.database().ref().child("contest_players/" + id)
+            .orderByChild("id").equalTo(this.state.user_id).once("value", snap => {
+                snap.forEach(child => {
+                    if (child.val().id === this.state.user_id) {
+                        joined = true
+                    }
+                })
+            }).then(() => {
+                let com = this
+                if (joined) {alert("you have already joined this league")}
+                else {
+                    firebase.database().ref().child("profiles/" + com.state.user_id).once('value', snap => {
+                        let before_coins = snap.val().coins;
+                        if (entry > before_coins) {
+                            alert("you dont have enough coin please buy some")
+                        } else {
+                            var answer = window.confirm(entry + " coins will be deduct from " + before_coins + " coins");
+                            if (answer) {
+                                let after_coins = before_coins - entry
+                                var up = {}
+                                up["/profiles/" + com.state.user_id + "/coins"] = after_coins
+                                firebase.database().ref().update(up).then(() => {
+                                    firebase.database().ref().child("contest_players/" + id).push().set({ id: com.state.user_id })
+                                    window.location.reload();
+                                }).catch(error => { alert(error) })
+                            } else {
+                            }
+                        }
+                    })
+                }
+            })
     }
     render() {
         return (
 
             <div className="container">
                 <Navbar />
-                <br/>
+                <br />
                 {this.state.contests.reverse()}
-                {this.state.contests.map(items=>(
+                {this.state.contests.map((items, key) => (
                     <div>
-                <div className="card">
-                    <div className="carditems"><img src={items.map==="Miramaar"?Miramar:Erangel} alt="hola" width="110" height="70"/></div>
-                    <div className="carditems"> <p className="itemHead" style={{marginTop: "5px"}}>Type</p><p>{items.type}</p></div>
-                    <div className="carditems"> <p className="itemHead" style={{marginTop: "5px"}}>Date</p><p>{items.date}</p></div>
-                    <div className="carditems"> <p className="itemHead" style={{marginTop: "5px"}}>Time</p><p>{items.time}</p></div>
-                    <div className="carditems"> <p className="itemHead" style={{marginTop: "5px"}}>Entry</p><p>{items.entry}</p></div>
-                    <div className="carditems"> <p className="itemHead" style={{marginTop: "5px"}}>Per kill</p><p>{items.per_kill}</p></div>
-                    <div className="carditems"> <p className="itemHead" style={{marginTop: "5px"}}>Players</p><p>a</p></div>
-                    <div className="carditems"><button className="button btn-info">Pay Now</button></div>
-                </div><br/></div>
-))}
-<br/>
-<Footer/>
-            </div>  
+                        <div className="card" key={items.id}>
+                            <div className="carditems"><img src={items.map === "Miramaar" ? Miramar : Erangel} alt="hola" width="110" height="70" /></div>
+                            <div className="carditems"> <p className="itemHead" style={{ marginTop: "5px" }}>Type</p><p>{items.type}</p></div>
+                            <div className="carditems"> <p className="itemHead" style={{ marginTop: "5px" }}>Date</p><p>{items.date}</p></div>
+                            <div className="carditems"> <p className="itemHead" style={{ marginTop: "5px" }}>Time</p><p>{items.time}</p></div>
+                            <div className="carditems"> <p className="itemHead" key={items.id} style={{ marginTop: "5px" }}>Entry</p><p>{items.entry}</p></div>
+                            <div className="carditems"> <p className="itemHead" style={{ marginTop: "5px" }}>Per kill</p><p>{items.per_kill}</p></div>
+                            <div className="carditems"> <p className="itemHead" style={{ marginTop: "5px" }}>Players</p><p>a</p></div>
+                            <div className="carditems"><button className="button btn-info" key={items.id} onClick={this.Join.bind(this, items.entry, items.id)}>{this.state.join}</button></div>
+                        </div><br /></div>
+                ))}
+                <br />
+                <Footer />
+            </div>
         );
     }
 }
